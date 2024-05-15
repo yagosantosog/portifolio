5b0fa5c304cbcb5cae61ded9c71faca276169846
yagosantosog <santosyago2005@gmail.com>
2024-05-15 15:58:19 -0300

    change button styles

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -4,20 +4,50 @@
 
 <style scoped>
 button {
+  position: relative;
   font-size: var(--step-3);
   max-width: fit-content;
   padding: 0.2em 0.5em;
   background-color: hsl(var(--clr-secundary));
+
   border: none;
   border-radius: 0.2em;
-  color: hsl(var(--clr-main));
+  color: hsl(var(--clr-pure-white));
   font-weight: bold;
-  box-shadow: 0.3em 0.3em 0.3em hsl(var(--clr-pure-black) / 0.2);
   transition: transform 0.4s;
+  cursor: pointer;
 }
 
 button:hover {
-  transform: translateY(-5px);
-  cursor: pointer;
+  color: hsl(var(--clr-secundary));
+  background: transparent;
+  box-shadow: none;
+}
+
+button:before,
+button:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 2px;
+  width: 0;
+  background: hsl(var(--clr-secundary));
+  box-shadow:
+    -1px -1px 5px 0px #fff,
+    7px 7px 20px 0px #0003,
+    4px 4px 5px 0px #0002;
+  transition: 400ms ease all;
+}
+button:after {
+  right: inherit;
+  top: inherit;
+  left: 0;
+  bottom: 0;
+}
+button:hover:before,
+button:hover:after {
+  width: 100%;
+  transition: 800ms ease all;
 }
 </style>
